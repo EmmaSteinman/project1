8f056af994252f960ec52d842d44478db167417d
benliepert <ben.liepert@gmail.com>
2018-09-19 15:11:28 -0400

    added malloc() for thread's sema member. Tests failing by timeout

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -104,6 +104,7 @@ timer_sleep (int64_t ticks) // 1 tick = 1/100th of a second
   cur->wakeAt = start + ticks;       //set wakeAt so thread "sleeps" and will be
                                       //checked by thread_ticks()
   // use semaphore here
+  cur->sleepSema = malloc(sizeof(struct semaphore));
   sema_init(cur->sleepSema, 1);
   sema_down(cur->sleepSema); // calling thread is blocked now, we'll come back when time is up
 }
