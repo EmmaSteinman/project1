27ffe4b3c98465b45ac7f4a9b6ba28f75c3becf2
Emma Steinman <steinm_e1@red9.mathsci.denison.edu>
2018-09-18 23:11:53 -0400

    Starting to implement alarm clock 9/18

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,6 +89,8 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    int64_t wakeAt;                     /* If nonnegative, thread is sleeping
+                                          and should wake up at wakeAt ticks */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
